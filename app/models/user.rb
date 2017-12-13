6d05b11eb904f79538508e44962867b9598e93ac
larrytm1 <lawrencemurry@yahoo.com>
2017-12-13 08:36:40 +0000

    Fixing NoMethodError undefined method 'login_links'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, 
-         :confirmable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
          
   validates :fullname, presence: true, length: {maximum: 50}
   
