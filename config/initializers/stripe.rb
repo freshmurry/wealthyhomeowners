9583a17631050dd9de7dd5150f486a28f8e3a6e6
larrytm1 <lawrencemurry@yahoo.com>
2017-12-18 06:37:35 +0000

    Masked Stripe Secret Keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,8 +2,9 @@ Rails.configuration.stripe = {
   #---- TEST ----
   # :publishable_key => 'pk_test_SZlZj99vzoclqVZRRLswAQ8h',
   # :secret_key => 'sk_test_f4TIZbC578AuDRAuSQwIXhIg'
+  
   #---- LIVE ----
-  :publishable_key => 'pk_live_5W1sDTDyDdGGd5QtelSvq2yZ',
-  :secret_key => 'sk_live_6bAj5v6njneQEc7hbx6Hn99Z'
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
