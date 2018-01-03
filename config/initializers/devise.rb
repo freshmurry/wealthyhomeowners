82e93078cc5cebcd6504bfb629513f90756c9e98
larrytm1 <lawrencemurry@yahoo.com>
2018-01-03 05:59:44 +0000

    Configured Facebook Sign Up to work on Heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -276,6 +276,7 @@ Devise.setup do |config|
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
   config.omniauth :facebook, '1518262284959364', '309d726626a5e3d8156e7b98c693b966', scope: 'email', info_fields: 'email, name'
+
   #----- STRIPE TEST -----
   # config.omniauth :stripe_connect, 'ca_Bz12s2Z5ijkGknATCnWx9EmDZIvGMf0e', 'sk_test_uQnVqs5Vnt9upqbJJNCSmiYy', scope: 'read_write', stripe_landing: 'login'
   
