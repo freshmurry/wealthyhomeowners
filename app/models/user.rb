6ab67309263d9a1552c13fa88542797fce927413
larrytm1 <lawrencemurry@yahoo.com>
2018-01-05 00:54:46 +0000

    Fixed issue with Facebook Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
     if user
       return user
     else
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |u|
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
         user.fullname = auth.info.name
