109effa0c808585fad356f51d71f860db71583a7
larrytm1 <lawrencemurry@yahoo.com>
2018-01-07 04:06:58 +0000

    Action mailer gmail integration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,7 +116,7 @@ Rails.application.configure do
   #   authentication: 'plain'
   # }
   
-  config.action_mailer.default_url_options = { host: 'http://shindigspace.lawrencemurry.com' }
+  config.action_mailer.default_url_options = { host: 'http://shindigspace.lawrencemurry.com', port: 8080 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
