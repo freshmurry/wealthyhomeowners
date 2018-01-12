8faab2306929d6462d335e078de8fb264f534759
larrytm1 <lawrencemurry@yahoo.com>
2018-01-12 20:39:24 +0000

    Fixed paperclip AWS ENV Vars for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,12 @@
 Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
+    path: ':class/:attachment/:id/:style/:filename',
     :s3_credentials => {
-    :s3_region => 'us-east-1',
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :s3_region => ENV['AWS_REGION']
     }
   }
   
