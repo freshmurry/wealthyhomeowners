55c71053fa9cc13fde0f95b7827803e68a8fb5d5
larrytm1 <lawrencemurry@yahoo.com>
2018-02-28 00:05:27 +0000

    Pushing to Github

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,10 +1,10 @@
 Rails.configuration.stripe = {
   #---- TEST ----
-  :publishable_key => 'pk_test_WYMTzyGZ9KXIM7ckP4mIDQEa',
-  :secret_key => 'sk_test_uQnVqs5Vnt9upqbJJNCSmiYy'
+  # :publishable_key => 'pk_test_WYMTzyGZ9KXIM7ckP4mIDQEa',
+  # :secret_key => 'sk_test_uQnVqs5Vnt9upqbJJNCSmiYy'
   
   #---- LIVE ----
-  # :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  # :secret_key      => ENV['STRIPE_SECRET_KEY']
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
