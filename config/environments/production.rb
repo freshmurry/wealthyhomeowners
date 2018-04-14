ae49db02414404c53169f1724c0722368c82b81c
larrytm1 <lawrencemurry@yahoo.com>
2018-04-14 15:20:17 +0000

    Cleaning asset pipeline to work in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
+  config.action_controller.perform_caching = true
   
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -32,9 +33,10 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
-  config.assets.initialize_on_precompile = false
-  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  # config.public_file_server.enabled = true
+  # config.assets.initialize_on_precompile = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
