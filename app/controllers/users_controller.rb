4ffed3ab8e3db1f64aee4818855d1e294ac42463
larrytm1 <lawrencemurry@yahoo.com>
2018-08-29 06:55:00 +0000

    Updated login_links to login_link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   def payout
     if !current_user.merchant_id.blank?
       account = Stripe::Account.retrieve(current_user.merchant_id)
-      @login_link = account.login_links.create()
+      @login_link = account.login_link.create()
     end
   end
 
