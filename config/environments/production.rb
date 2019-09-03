b79130ede85f6c6cdb01b5e2577db436791e233b
Lawrence Murry <lawrencemurry@yahoo.com>
2019-09-03 01:41:55 -0500

    Fixed misspelling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,11 +32,11 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
-  config.assets.js_compressor = Uglifier.new(harmoney: true)
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = ture
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
