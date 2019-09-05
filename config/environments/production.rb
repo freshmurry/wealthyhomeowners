87a8b3b38112666c2f741deff3f6fd2c3581d11b
Lawrence Murry <lawrencemurry@yahoo.com>
2019-09-05 04:13:04 -0500

    Setting redistogo for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = "wss://action-cable-shindigspace.com/cable"
+  config.action_cable.allowed_request_origins = ['https://action-cable-shindigspace.com/cable', 'https://action-cable-shindigspace.com/cable' ]
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
