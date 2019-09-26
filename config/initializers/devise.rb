d84862b2da8ee4a2d602774e931568b994ae3d8c
Lawrence Murry <lawrencemurry@yahoo.com>
2019-09-25 23:09:14 -0500

    Trying to get FB omniauth to work in production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -277,10 +277,10 @@ Devise.setup do |config|
 
   config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], scope: 'email', info_fields: 'email, name'
   # config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], :display => 'popup', scope: 'email', info_fields: 'email, name',
-    # client_options: {
-    #                 site: 'https://graph.facebook.com/v2.11',
-    #                 authorize_url: "https://www.facebook.com/v2.11/dialog/oauth"
-    #             }
+    client_options: {
+                    site: 'https://graph.facebook.com/v2.11',
+                    authorize_url: "https://www.facebook.com/v2.11/dialog/oauth"
+                }
 
   #----- STRIPE TEST -----
   # config.omniauth :stripe_connect, 'ca_Bz12s2Z5ijkGknATCnWx9EmDZIvGMf0e', 'sk_test_uQnVqs5Vnt9upqbJJNCSmiYy', scope: 'read_write', stripe_landing: 'login'
