bdc493041c5ccbb73ed0d43934e6440c8718182d
Lawrence Murry <lawrencemurry@yahoo.com>
2019-09-27 01:57:18 -0500

    Trying to get FB omniauth to work in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ApplicationRecord
     if user
       return user
     else
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      where(provider: auth.provider, uid: auth.uid, imaage: auth.info.image).first_or_create do |user|
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
         user.fullname = auth.info.name
