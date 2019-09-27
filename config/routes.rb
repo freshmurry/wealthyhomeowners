ebf92b9f59a3a66a9ae0573269d30787c0e75fb2
Lawrence Murry <lawrencemurry@yahoo.com>
2019-09-27 00:07:25 -0500

    Trying to get FB omniauth to work in production

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
               path: '',
               path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
               # controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
-              :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'registrations' }
+              :controllers => { :omniauth_callbacks => "users/auth/facebook/callback", registrations: 'registrations' }
  		
    resources :users, only: [:show] do
      member do
