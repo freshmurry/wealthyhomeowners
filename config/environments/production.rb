fe402ce4c323f3bae406c0cf61ea11b62ccf2bc0
Lawrence Murry <lawrencemurry@yahoo.com>
2019-10-13 22:28:58 -0500

    trying to fix paperclip issue in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,12 +1,13 @@
 Rails.application.configure do
   config.paperclip_defaults = {
-    :storage => :s3,
+    storage: :s3,
     s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :s3_region => ENV['AWS_REGION']
+    url: ":s3_domain_url"
+    s3_credentials: {
+      bucket: ENV.fetch['S3_BUCKET_NAME'],
+      access_key_id: ENV.fetch['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV.fetch['AWS_SECRET_ACCESS_KEY'],
+      s3_region: ENV.fetch['AWS_REGION']
     }
   }
   
@@ -36,7 +37,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
