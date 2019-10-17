999a6070a622460c3b471c243e4cbd7a46242b80
Lawrence Murry <lawrencemurry@yahoo.com>
2019-10-17 00:29:20 -0500

    Fresh Start

--- db/schema.rb
+++ db/schema.rb
@@ -127,7 +127,7 @@ ActiveRecord::Schema.define(version: 20180109174910) do
     t.integer  "unread",                 default: 0
     t.string   "image_file_name"
     t.string   "image_content_type"
-    t.bigint   "image_file_size"
+    t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
