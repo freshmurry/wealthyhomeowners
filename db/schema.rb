20f35907ed5fb685c1267714eaf1d46fa59f725f
Lawrence <lawrencemurry@yahoo.com>
2020-02-21 04:39:53 +0000

    Fixed database not rendering homes latiude

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20180109174910) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_homes_on_user_id"
   end
 
